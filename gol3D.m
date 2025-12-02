1e0df48cdab754308f6aaa2fc27bdb393687f876
Gyorgy0 <lukovics.gyuri@gmail.com>
2025-12-02 13:01:53 +0100

    Added more robust rule system

--- gol3D.m
+++ gol3D.m
@@ -1,9 +1,13 @@
 width = 0.5;
-length = 15;
-cells = randi([0 1], length,length,length);
-cells(6,6,1) = 1;
-cells(6,7,1) = 1;
-cells(6,5,1) = 1;
+length = 31;
+cells = zeros(length,length,length);
+for i = -1:1
+    for j = -1:1
+        for k = -1:1
+            cells(round(length/2) + i,round(length/2) + j,round(length/2) + k) = 1;
+        end
+    end
+end
 color = jet(length);
 fig = figure("Name","3D Game of Life");
 % Initialize the 3D plot
@@ -15,13 +19,20 @@ title('3D Game of Life Visualization');
 grid on;
 view(3)
 cell_voxels = zeros(length,length,length);
+% 0 - Moore neighborhood (6 cells checked)
+% 1 - radial neighborhood
+% 3 - von Neumann neighborhood
+neighborhood = 2;
+% Rules
+survival = [1 4 5 6 7 8 9 10 11 12];
+birth = [2 4 5 6];
 
 for i = 1:size(cells,1)
     for j = 1:size(cells,2)
         for k = 1:size(cells,3)
-            [x,y,z] = setxyz(i,j,k, 0.5 - (k*0.01));
+            [x,y,z] = cube(i,j,k, 0.5 - (k*0.01));
             hold on;
-            cell_voxels(i,j,k) = surf(x,y,z, 'FaceColor', color(k,:));
+            cell_voxels(i,j,k) = surface(x,y,z, 'FaceColor', color(k,:));
             hold off;
         end
     end
@@ -119,21 +130,16 @@ for t = 1:120
                             neighbours=neighbours+1;
                         end
                     end
-                    % 26 checked cell neighbours
-                    % Fewer than min_nghb neighbours -> cell dies
-                    % Min_nghb-max_nghb living neighbours -> cell lives
-                    % More than max_nghb living neighbours -> cell dies
-                    % Max_nghb living neighbours -> cell becomes a live cell
-                    min_nghb = 6;
-                    max_nghb = 12;
-                    if (neighbours < min_nghb)
-                        cells(i,j,k) = 0; % Cell dies
-                    elseif (neighbours >= min_nghb && neighbours <= max_nghb && cells(i,j,k) == 1)
-                        cells(i,j,k) = 1; % Cell lives
-                    elseif (neighbours == max_nghb && cells(i,j,k) == 0)
-                        cells(i,j,k) = 1; % Cell becomes a live cell
-                    else
-                        cells(i,j,k) = 0; % Cell dies
+                    cells(i,j,k) = 0; % Cell dies
+                    for s = 1:size(survival,1)
+                        if (neighbours == survival(s))
+                        cells(i,j,k) = cells(i,j,k); % Cell lives
+                        end
+                    end
+                    for b = 1:size(birth,1)
+                        if (neighbours == birth(b))
+                        cells(i,j,k) = 1; % New cell is born
+                        end
                     end
             end
         end
@@ -160,7 +166,7 @@ for t = 1:120
 end
 
 
-function [x,y,z] = setxyz(posx,posy,posz,width)
+function [x,y,z] = cube(posx,posy,posz,width)
 x = [ posx+width  posx+width  posx+width  posx+width  posx+width; posx+width  posx+width posx-width posx-width  posx+width;posx+width  posx+width posx-width posx-width posx+width;posx+width  posx+width  posx+width posx+width posx+width];
 y = [ posy+width posy-width posy-width  posy+width  posy+width; posy+width posy-width posy-width  posy+width  posy+width;posy+width posy-width posy-width  posy+width posy+width;posy+width posy-width posy-width posy+width posy+width];
 z = [posz-width posz-width posz-width posz-width posz-width;posz-width posz-width posz-width posz-width posz-width;posz+width  posz+width  posz+width  posz+width posz+width;posz+width  posz+width  posz+width posz+width posz+width];    
